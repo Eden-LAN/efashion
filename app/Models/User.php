ce3d3dd74600dde5edc25f1efbf3e345ad8c3615
Eden Lan <lanailin4@gmail.com>
2022-06-23 19:19:03 +0200

    created models

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Favorite;
+use App\Review;
 
 class User extends Authenticatable
 {
@@ -41,4 +43,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function favorites()
+    {
+      return $this->hasMany(Favorite::class);
+    }
+
+    public function reviews()
+    {
+      return $this->hasManyThrough(Review::class, Favorite::class);
+    }
 }
--- database/migrations/2022_06_23_155143_create_items_table.php
+++ database/migrations/2022_06_23_155143_create_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateItemsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-      if (!Schema::hasTable('users')) {
-        Schema::create('users', function (Blueprint $table) {
+      if (!Schema::hasTable('items')) {
+        Schema::create('items', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('id');
+            $table->string('brand');
+            $table->string('color');
+            $table->string('gender');
+            $table->string('category');
+            $table->string('img');
             $table->timestamps();
         });
       }
@@ -33,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('items');
     }
 }
